7c3919d589da6f7a7790691510b71f7a1c20706e
yavordobrev24 <yavordobrev24@gmail.com>
2024-03-01 20:17:57 +0200

    Updated to match eeg device signals

--- frontend/src/components/AnswerQuestion/AnswerQuestion.tsx
+++ frontend/src/components/AnswerQuestion/AnswerQuestion.tsx
@@ -13,32 +13,37 @@ interface AnsweredQuestion {
   answer: string;
 }
 
-const Q1_THRESHOLD = 0.5;
+const Q1_THRESHOLD = 0;
 
 const AnswerQuestions = () => {
   const [questionObj, setQuestionObj] = useState<Question | null>(null);
-  const [answeredQuestions, setAnsweredQuestions] = useState<AnsweredQuestion[]>([]);
+  const [answeredQuestions, setAnsweredQuestions] = useState<
+    AnsweredQuestion[]
+  >([]);
   const navigate = useNavigate();
-  
+
   // Refs for the answer buttons
   const leftButtonRef = useRef<HTMLButtonElement>(null);
   const rightButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const storedQuestionsString = sessionStorage.getItem("questions");
-  
+
     if (storedQuestionsString) {
       const storedQuestions: Question[] = JSON.parse(storedQuestionsString);
-  
+
       if (storedQuestions.length > 0) {
         setQuestionObj(storedQuestions[0]);
       } else {
         sessionStorage.removeItem("questions");
-        sessionStorage.setItem("answeredQuestions", JSON.stringify(answeredQuestions));
+        sessionStorage.setItem(
+          "answeredQuestions",
+          JSON.stringify(answeredQuestions)
+        );
         navigate("/answer-summary");
       }
     }
-  
+
     const fetchData = async () => {
       try {
         const response = await fetch(file);
@@ -46,45 +51,43 @@ const AnswerQuestions = () => {
         const lines = data.split("\n");
         const lastestData = lines[lines.length - 2];
         const parsedData = JSON.parse(lastestData);
-        const Q1 = parsedData.mot[4];
-        if (Math.abs(Q1) > Q1_THRESHOLD) {
-          if (Q1 > 0) {
-            console.log("Right");
-            // Hover over the right button
-            rightButtonRef.current?.classList.add(styles.hovered);
-            // Set a timeout to click the button after 2 seconds
-            setTimeout(() => {
-              rightButtonRef.current?.click();
-            }, 2000);
-          } else {
-            console.log("Left");
-            // Hover over the left button
-            leftButtonRef.current?.classList.add(styles.hovered);
-            // Set a timeout to click the button after 2 seconds
-            setTimeout(() => {
-              leftButtonRef.current?.click();
-            }, 1000);
-          }
+        const Q1 = parsedData.mot[3];
+
+        if (Q1 < Q1_THRESHOLD) {
+          console.log("Right");
+          // Hover over the right button
+          rightButtonRef.current?.classList.add(styles.hovered);
+          leftButtonRef.current?.classList.remove(styles.hovered);
+          // Set a timeout to click the button after 2 seconds
+          setTimeout(() => {
+            //   rightButtonRef.current?.click();
+          }, 3000);
+        } else {
+          console.log("Left");
+          // Hover over the left button
+          leftButtonRef.current?.classList.add(styles.hovered);
+          rightButtonRef.current?.classList.remove(styles.hovered);
+          // Set a timeout to click the button after 2 seconds
+          setTimeout(() => {
+            //   leftButtonRef.current?.click();
+          }, 3000);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-  
+
     // Fetch data initially
     fetchData();
-  
+
     // Schedule subsequent requests every 1 second
     const fetchDataInterval = setInterval(fetchData, 1000);
-  
+
     return () => {
       // Clear the interval when the component is unmounted
       clearInterval(fetchDataInterval);
     };
-  
   }, [answeredQuestions, navigate]);
-  
-  
 
   const handleAnswerClick = (selectedAnswer: string) => {
     if (questionObj) {
@@ -124,10 +127,16 @@ const AnswerQuestions = () => {
         <>
           <h2>{questionObj.question}</h2>
           <div className={styles["btn-group"]}>
-            <button ref={leftButtonRef} onClick={() => handleAnswerClick(questionObj.answers[0])}>
+            <button
+              ref={leftButtonRef}
+              onClick={() => handleAnswerClick(questionObj.answers[0])}
+            >
               {questionObj.answers[0]}
             </button>
-            <button ref={rightButtonRef} onClick={() => handleAnswerClick(questionObj.answers[1])}>
+            <button
+              ref={rightButtonRef}
+              onClick={() => handleAnswerClick(questionObj.answers[1])}
+            >
               {questionObj.answers[1]}
             </button>
           </div>
